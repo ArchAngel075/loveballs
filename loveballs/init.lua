208a1621383ba0b5e1957494cff9139a62135174
ArchAngel075 <archangel07555@gmail.com>
2015-02-05 06:06:19 +0200

    modified setRadius function and updated to original.
    forks closer to shorefire master branch than ArchAngel075 master branch
    
    setRadius now uses raytraces to prevent clipping.
    setRadius now adjusts centerShape instead of overwrites, this solves the deletion of physics data for the softbody
    
    attempts at preserving average node physics data between radius shifts fail due to raytrace lineLength < 0.0f errors.

--- loveballs/init.lua
+++ loveballs/init.lua
@@ -1,137 +1,244 @@
---Original Softbody lib by Shorefire/Steven
---Modifications made by ArchAngel075/Jaco
+--Softbody lib by Shorefire/Steven
 --tesselate function by Amadiro/Jonathan Ringstad
+local path = ...;
 
-require "loveballs/class"
+require(path.."/class");
 
-Sbody = newclass("Sbody");
-function Sbody:init(world, x, y, r, s, tess)
+Softbody = newclass("Softbody");
+function Softbody:init(world, x, y, r, s, t)
+	--create center body
+  self.world = world
+	self.centerBody = love.physics.newBody(world, x, y, "dynamic");
+	self.centerShape = love.physics.newCircleShape(r/4);
+	self.centerfixture = love.physics.newFixture(self.centerBody, self.centerShape);
+	self.centerfixture:setMask(1);
+  
+  --create 'nodes'
+  self:constructNodes(x, y, r)
+  
+  --set tesselation and smoothing
+  if s > 2 then
+		s = 2;
+	end
 	self.smooth = s or 2;
+
+	local tess = t or 4;
 	self.tess = {};
-	local tess = tess or 2;
-	for i=1, tess do
+	for i=1,tess do
 		self.tess[i] = {};
 	end
 
-	local world = world;
-	self.sourceBody = love.physics.newBody(world, x, y, "dynamic");
-	self.sourceShape = love.physics.newCircleShape(r/4);
-	self.fixture = love.physics.newFixture(self.sourceBody, self.sourceShape);
+	self.dead = false;
+end
 
-	self.fixture:setMask(1);
-
-	self.nodeShape = love.physics.newCircleShape(6);
+function Softbody:constructNodes(x, y, r)
+  --create 'nodes' (outer bodies) & connect to center body
+  local world = self.world
+	self.nodeShape = love.physics.newCircleShape(8);
 	self.nodes = {};
 
 	local nodes = r/2;
 
 	for node = 1, nodes do
 		local angle = (2*math.pi)/nodes*node;
+		
 		local posx = x+r*math.cos(angle);
 		local posy = y+r*math.sin(angle);
+    posx, posy = self:rayCastNode(posx,posy,r,angle)
+
 		local b = love.physics.newBody(world, posx, posy, "dynamic");
+		b:setAngularDamping(50);
+		
 		local f = love.physics.newFixture(b, self.nodeShape);
 		f:setFriction(30);
 		f:setRestitution(0);
-		b:setAngularDamping(50);
-		
-		local j = love.physics.newDistanceJoint(self.sourceBody, b, posx, posy, posx, posy, true);
+		local j = love.physics.newDistanceJoint(self.centerBody, b, posx, posy, posx, posy, false);
 		j:setDampingRatio(0.1);
 		j:setFrequency(12*(20/r));
 
 		table.insert(self.nodes, {body = b, fixture = f, joint = j});
 	end
 
+
+	--connect nodes to eachother
 	for i = 1, #self.nodes do
 		if i < #self.nodes then
 			local j = love.physics.newDistanceJoint(self.nodes[i].body, self.nodes[i+1].body, self.nodes[i].body:getX(), self.nodes[i].body:getY(),
 			self.nodes[i+1].body:getX(), self.nodes[i+1].body:getY(), false);
 			self.nodes[i].joint2 = j;
+		else
+			local j = love.physics.newDistanceJoint(self.nodes[i].body, self.nodes[1].body, self.nodes[i].body:getX(), self.nodes[i].body:getY(),
+			self.nodes[1].body:getX(), self.nodes[1].body:getY(), false);
+			self.nodes[i].joint3 = j;
 		end
 	end
-
-	local i = #self.nodes;
-	local j = love.physics.newDistanceJoint(self.nodes[i].body, self.nodes[1].body, self.nodes[i].body:getX(), self.nodes[i].body:getY(),
-	self.nodes[1].body:getX(), self.nodes[1].body:getY(), false);
-	self.nodes[i].joint3 = j;
-
-	self.dead = false;
 end
 
-function Sbody:update()
-  if not self.dead then
-		local pos = {};
-		for i = 1, #self.nodes, self.smooth do
-			v = self.nodes[i];
-			table.insert(pos, v.body:getX());
-			table.insert(pos, v.body:getY());
-		end
+function Softbody:rayCastNode(nodeX,nodeY,r,angle)
+  --takes nodeX,nodeY and casts from center sourceBody
+  --returns the first/closest contact
+  
+  local center = {self.centerBody:getPosition()}
+  local px,py = nodeX, nodeY
+  local rayCast_array = {}
+  
+  local loop = true
+  local hitList = {}
+  
+  local function worldRayCastCallback(fixture, x, y, xn, yn, fraction)
+    local hit = {}
+    
+    if fixture == self.centerfixture then return 1 end --special case
+    local case2
+    for i,o in pairs(self.nodes) do
+      if fixture == o.fixture then
+        case2 = true
+        break
+      end
+    end
+    if case2 then return 1 end
+    
+    hit.fixture = fixture
+    hit.x, hit.y = x, y
+    hit.xn, hit.yn = xn, yn
+    hit.fraction = fraction
 
-		tessellate(pos, self.tess[1]);
-		for i=1,#self.tess - 1 do
-			tessellate(self.tess[i], self.tess[i+1]);
-		end
-    
-    return self.tess[#self.tess]
-    
+    table.insert(hitList, hit)
+
+    return 1 -- Continues with ray cast through all shapes.
   end
+  
+  --center[1], center[2], nodeX, nodeY
+  
+  if math.sqrt( ( center[1] - nodeX ) * ( center[1] - nodeX ) + ( center[2] - nodeY ) * ( center[2] - nodeY ) ) <= 0.0 then
+    return px,py
+  end
+  
+  self.world:rayCast( center[1], center[2], nodeX, nodeY, worldRayCastCallback)
+  
+  if #hitList > 0 then
+    --get closest fixture
+    local closest = nil
+    local closestDX = nil
+    for k,v in pairs(hitList) do
+      if not closest then
+        closestDX = math.sqrt( ( center[1] - nodeX ) * ( center[1] - nodeX ) + ( center[2] - nodeY ) * ( center[2] - nodeY ) )
+        closest = k
+      else
+        local dx = math.sqrt( ( center[1] - v.x ) * ( center[1] - v.x ) + ( center[2] - v.y ) * ( center[2] - v.y ) )
+        if dx < closestDX then
+          closestDX = dx
+          closest = k
+        end
+      end
+    end
+    --print(hitList[closest])
+    px,py = hitList[closest].x,hitList[closest].y
+  end
+  return px,py
 end
 
-function Sbody:destroy()
-	if not self.dead then
-		for i = #self.nodes, 1, -1 do
-			self.nodes[i].body:destroy();
-			self.nodes[i] = nil;
-		end
+function Softbody:update()
+	--update tesselation (for drawing)
+	local pos = {};
+	for i = 1, #self.nodes, self.smooth do
+		v = self.nodes[i];
 
-		self.sourceBody:destroy();
-		self.dead = true;
+		table.insert(pos, v.body:getX());
+		table.insert(pos, v.body:getY());
+	end
+
+	tessellate(pos, self.tess[1]);
+	for i=1,#self.tess - 1 do
+		tessellate(self.tess[i], self.tess[i+1]);
 	end
 end
 
-function Sbody:setFrequency(f)
+function Softbody:destroy()
+	if self.dead then
+		return;
+	end
+
+	for i = #self.nodes, 1, -1 do
+		self.nodes[i].body:destroy();
+		self.nodes[i] = nil;
+	end
+
+	self.sourceBody:destroy();
+	self.dead = true;
+end
+
+function Softbody:setFrequency(f)
 	for i,v in pairs(self.nodes) do
 		v.joint:setFrequency(f);
 	end
 end
 
-function Sbody:getFrequency()
-  local count = 0
-	for i,v in pairs(self.nodes) do
-		count = count+v.joint:getFrequency();
-	end
-  return count/#self.nodes
-end
-
-function Sbody:setDamping(d)
+function Softbody:setDamping(d)
 	for i,v in pairs(self.nodes) do
 		v.joint:setDampingRatio(d);
 	end
 end
 
-function Sbody:draw(type, width)
-	if not self.dead then
-		if not width then
-			width = 20;
-		end
-    
-    self:update() --update tessellation
-
-		love.graphics.setLineStyle("smooth");
-		love.graphics.setLineWidth(width);
-
-		if type == "line" then
-			love.graphics.polygon("line", self.tess[#self.tess]);
-		else
-			love.graphics.polygon("fill", self.tess[#self.tess]);
-			love.graphics.polygon("line", self.tess[#self.tess]);
-		end
+function Softbody:setFriction(f)
+	for i,v in ipairs(self.nodes) do
+		v.fixture:setFriction(0);
 	end
-  love.graphics.setLineWidth(1)
 end
 
-function Sbody:getPoints()
- return self.tess[#self.tess]
+function Softbody:getPoints()
+	return self.tess[#self.tess];
+end
+
+function Softbody:getRadius()
+  return self.centerShape:getRadius()*4
+end
+
+function Softbody:setRadius(r)
+  if self.dead then
+		return;
+	end
+  
+	for i = #self.nodes, 1, -1 do
+		self.nodes[i].body:destroy();
+		self.nodes[i] = nil;
+	end
+  
+  self.centerShape:setRadius(r/4)
+  
+  self:constructNodes(self.centerBody:getX(), self.centerBody:getY(), r)
+  
+  local tess = t or 4;
+	self.tess = {};
+	for i=1,tess do
+		self.tess[i] = {};
+	end
+  
+  self:update()
+end
+
+function Softbody:draw(type, debug)
+	if self.dead then
+		return;
+	end
+
+	love.graphics.setLineStyle("smooth");
+	love.graphics.setLineWidth(self.nodeShape:getRadius()*2);
+
+	if type == "line" then
+		love.graphics.polygon("line", self.tess[#self.tess]);
+	else
+		love.graphics.polygon("fill", self.tess[#self.tess]);
+		love.graphics.polygon("line", self.tess[#self.tess]);
+	end
+
+	love.graphics.setLineWidth(1);
+
+	if debug then
+		for i,v in ipairs(self.nodes) do
+			love.graphics.circle("line", v.body:getX(), v.body:getY(), self.nodeShape:getRadius());
+		end
+	end
 end
 
 --tessellate function by Amadiro/Jonathan Ringstad
